e769b0b9ba4ac5b9cac0ca05d15d86bb5269ee4c
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-07 20:43:33 -0300

    Add starter app to start working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-function App() {
+function App({ notes }) {
+  console.log(notes)
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Notes App</h2>
+      {
+        notes.map(note => {
+          return <p key={note.id}>{note.content}</p>
+        })
+      }
     </div>
   )
 }
