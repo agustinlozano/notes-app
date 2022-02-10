4c768d1dff42e55244c5d69a72f313a9a0958c36
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-10 09:43:10 -0300

    Add new feature: filter notes by imprtance

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-function App({ notes }) {
+function App(props) {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+  const [showAll, setShowAll] = useState(false)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+    
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
   return (
     <div className="App">
       <h1>Notes App</h1>
+
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div> 
+
       <ul>
         {
-          notes.map(note => 
+          notesToShow.map(note => 
             <Note key={note.id} note={note} />
           )
         }
       </ul>
+      
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 const notes = [
   {
@@ -28,4 +28,4 @@ ReactDOM.render(
     <App notes={notes} />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
