4ab4b5cf89e3b01604c228b4e1cffc54c4759ca5
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-02 11:13:54 -0300

    Edit note service: now supports users' token

--- src/servises/notes.js
+++ src/servises/notes.js
@@ -1,11 +1,21 @@
 import axios from 'axios'
 
-const baseURL = 'https://stormy-falls-60730.herokuapp.com/api/notes'
+const baseURL = 'http://localhost:3001/api/notes'
+let token = null
 
+export const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
 export const getAll = () => axios.get(baseURL)
 
 export const create = newNote => {
-  const request = axios.post(baseURL, newNote)
+  const config = {
+    headers: {
+      Authorization: token
+    }
+  }
+  const request = axios.post(baseURL, newNote, config)
+
   return request.then(response =>
     console.log('New note added: ', response.data))
 }
