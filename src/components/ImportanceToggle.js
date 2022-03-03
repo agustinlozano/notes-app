cff84ca4d9d73e39176394b0732c2cd900d2cbb1
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-03 11:49:17 -0300

    Edit component: shorter syntax

--- src/components/ImportanceToggle.js
+++ src/components/ImportanceToggle.js
@@ -1,18 +1,15 @@
 import propTypes from 'prop-types'
 
-const ImportanceToggle = ({ setShowAll, showAll }) => {
-  return (
-    <div>
-      <button
-        onClick={() => setShowAll(!showAll)}
-        className='importance-toggle shadow'
-        id='btn'
-      >
-        show {showAll ? 'important' : 'all'}
-      </button>
-    </div>
-  )
-}
+const ImportanceToggle = ({ setShowAll, showAll }) =>
+  <div>
+    <button
+      onClick={() => setShowAll(!showAll)}
+      className='importance-toggle shadow'
+      id='btn'
+    >
+      show {showAll ? 'important' : 'all'}
+    </button>
+  </div>
 
 ImportanceToggle.propTypes = {
   setShowAll: propTypes.func.isRequired,
