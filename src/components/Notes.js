1f148daa05cc3dd6cc46d1a636ace48ab2348912
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-03 10:21:54 -0300

    Edit component

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,17 +1,16 @@
 import Note from './Note'
 
-const Notes = ({ notes }) => {
+const Notes = ({ notes }) =>
   <div className='notes shadow'>
     {
-        notes.map(note =>
-          <Note
-            key={note.id || note.content}
-            content={note.content}
-            user={note.user}
-          />
-        )
-      }
+      notes.map(note =>
+        <Note
+          key={note.id || note.content}
+          content={note.content}
+          user={note.user}
+        />
+      )
+    }
   </div>
-}
 
 export default Notes
